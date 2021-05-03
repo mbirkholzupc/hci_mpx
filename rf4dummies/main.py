b523a0a960a3f030bb1f1c6efdeff4a9eea29984
xaviercucurull <xavierqq@gmail.com>
2021-05-03 20:11:56 +0200

    Initial translations (#9)

--- rf4dummies/main.py
+++ rf4dummies/main.py
@@ -3,6 +3,9 @@
 # Purpose: Definitions of main app layout, navigation bar and settings
 ###############################################################################
 
+import gettext
+from os.path import dirname, join
+
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.properties import StringProperty
@@ -18,11 +21,58 @@ from kivymd.uix.list import MDList
 from kivymd.uix.list import OneLineListItem
 from kivymd.uix.list import OneLineIconListItem
 from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.list import OneLineAvatarIconListItem
+
+from kivy.lang import Observable
+from kivy.properties import StringProperty
 
 from reference import ReferencePage
 from training import TrainingPage
 from checklist import ChecklistPage
 from alignment import AntennaAlignmentHelperPage
+
+# https://github.com/tito/kivy-gettext-example/blob/master/main.py
+class Lang(Observable):
+    observers = []
+    lang = None
+
+    def __init__(self, defaultlang):
+        super(Lang, self).__init__()
+        self.ugettext = None
+        self.lang = defaultlang
+        self.switch_lang(self.lang)
+
+    def _(self, text):
+        return self.ugettext(text)
+
+    def fbind(self, name, func, args, **kwargs):
+        if name == "_":
+            self.observers.append((func, args, kwargs))
+        else:
+            return super(Lang, self).fbind(name, func, *args, **kwargs)
+
+    def funbind(self, name, func, args, **kwargs):
+        if name == "_":
+            key = (func, args, kwargs)
+            if key in self.observers:
+                self.observers.remove(key)
+        else:
+            return super(Lang, self).funbind(name, func, *args, **kwargs)
+
+    def switch_lang(self, lang):
+        # get the right locales directory, and instanciate a gettext
+        locales = gettext.translation('base', 'locales', languages=[lang])
+        self.ugettext = locales.gettext
+        self.lang = lang
+
+        # update all the kv rules attached to this text
+        for func, largs, kwargs in self.observers:
+            func(largs, None, None)
+            
+# Default language
+tr = Lang("en")
 
 # Extend Kivy classes
 class ItemDrawer(OneLineIconListItem):
@@ -46,11 +96,27 @@ class DrawerList(ThemableBehavior, MDList):
                 break
         instance_item.text_color = self.theme_cls.primary_color
 
+class ItemConfirm(OneLineAvatarIconListItem):
+    divider = None
+    
+    lang_dict = {'English': 'en', 'Català': 'ca', 'Français': 'fr', 'Español': 'es'}
+
+    def set_icon(self, instance_check):
+        tr.switch_lang(self.lang_dict[self.text])
+        instance_check.active = True
+        check_list = instance_check.get_widgets(instance_check.group)
+        for check in check_list:
+            if check != instance_check:
+                check.active = False
+                
 # Main Kivy Application
 class MainApp(MDApp):
     def build(self):
         self.i = 0  # clock counter
-
+        
+        self.lang_dialog = None
+        self.settings_dialog = None
+        
         self.theme_cls = ThemeManager()
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
@@ -71,12 +137,13 @@ class MainApp(MDApp):
         Clock.schedule_interval(self.clock_callback, 1)     # call every 1 second
 
         # Build settings menu
+        settings_dict = {'lang': tr._('Language'), 'settings': tr._('Settings')}
         settings_menu_items = [
           {
             "viewclass": "OneLineListItem",
-            "text": f"{item_name}",
-            "on_release": lambda x=f"{item_name}": self.settings_menu_callback(x),
-          } for item_name in ['Language', 'Settings']
+            "text": f"{settings_dict[item_id]}",
+            "on_release": lambda x=f"{item_id}": self.settings_menu_callback(x),
+          } for item_id in settings_dict
         ]
         self.settings_menu = MDDropdownMenu(
           caller=self.screen.ids.settings_menu_button,
@@ -99,9 +166,47 @@ class MainApp(MDApp):
             'Light' if self.theme_cls.theme_style == 'Dark' else 'Dark'
         )
         
-    def settings_menu_callback(self,instance_menu):
-      print(f'{instance_menu}')    
-      self.settings_menu.dismiss()
+    def settings_menu_callback(self,instance_menu):      
+      if instance_menu == 'lang':
+        self.settings_menu.dismiss()
+        self.show_confirmation_dialog()
+        
+    def close_lang_dialog(self, obj):
+      self.lang_dialog.dismiss()
+        
+    def close_settings_dialog(self, obj):
+      self.settings_dialog.dismiss()
+        
+    def show_confirmation_dialog(self):
+        ok_button = MDFlatButton(text=tr._('Ok'), on_release=self.close_lang_dialog)
+        if not self.lang_dialog:
+            self.lang_dialog = MDDialog(
+                auto_dismiss=True,
+                title=tr._('Select language'),
+                type="confirmation",
+                #events_callback = self.close_lang_select,
+                items=[
+                    ItemConfirm(text='English'),
+                    ItemConfirm(text='Català'),
+                    ItemConfirm(text='Français'),
+                    ItemConfirm(text='Español'),
+                ],
+                buttons=[ok_button],
+            )
+        self.lang_dialog.open()
+        
+    def show_settings_dialog(self):
+      ok_button = MDFlatButton(text=tr._('Ok'), on_release=self.close_settings_dialog)
+      if not self.settings_dialog:
+          self.settings_dialog = MDDialog(
+              auto_dismiss=True,
+              title=tr._('Settings'),
+              #type="confirmation",
+              #events_callback = self.close_lang_select,
+              buttons=[ok_button],
+          )
+      self.settings_dialog.open()
+
 
 if __name__ == '__main__':
     MainApp().run()
