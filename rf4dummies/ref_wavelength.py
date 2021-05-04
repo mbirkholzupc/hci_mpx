fd3f2788181b7c5038e894c33d5bc14ccd7ef583
Mike <michael.birkholz@estudiantat.upc.edu>
2021-05-04 21:55:58 +0200

    Create a content page and a simple layout base class

---
+++ rf4dummies/ref_wavelength.py
@@ -0,0 +1,10 @@
+from kivy.properties import StringProperty
+
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
+
+
+# Extend Kivy classes
+class RefWavelengthPage(MDBoxLayout):
+  pass
--- rf4dummies/reference.py
+++ rf4dummies/reference.py
@@ -3,12 +3,17 @@ from kivy.properties import StringProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.tab import MDTabsBase
+from kivy.uix.scrollview import ScrollView
+
+from ref_wavelength import RefWavelengthPage
 
 
 # Extend Kivy classes
 class ReferencePage(MDBoxLayout):
   pass
 
+class ReferenceArticlePicAndText(MDBoxLayout):
+  pass
 
 class RefTab(MDFloatLayout, MDTabsBase):
   content_text = StringProperty("")
